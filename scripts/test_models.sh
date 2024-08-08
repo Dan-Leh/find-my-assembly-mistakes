660ae497035a4ad6c53856d90a5bea634303bee5
Dan Lehman <dlehman@ln5117.cm.cluster>
2024-08-08 11:44:23 +0200

    evaluation script cleaned up and running

---
+++ scripts/test_models.sh
@@ -0,0 +1,16 @@
+#PBS -S /bin/bash
+#PBS -l select=1:ncpus=8:ngpus=1:gpu_type=v100:mem=32gb:dcloc=htc
+#PBS -N gca
+
+source activate /hpc/data/hpc-smc-internships/dlehman/python_envs/new
+
+cd /shared/nl011006/res_ds_ml_restricted/dlehman/state-diff-net
+
+for NAME in gca lca msa
+do
+    python test.py \
+    --config '/shared/nl011006/res_ds_ml_restricted/dlehman/state-diff-net/results/'$NAME'/config.yaml' \
+    --test_sets 'v2 extra' \
+    --log_iter 500 \
+    --save_fig_iter 20
+done
